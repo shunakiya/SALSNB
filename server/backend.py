6f078df4f3a3ca6195626ce9ffd46e87c05bbb29
Shun <143667220+shunakiya@users.noreply.github.com>
2025-03-31 22:01:45 -0700

    Delete server/backend.py

--- server/backend.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-backend_blueprint = Blueprint("backend_blueprint", __name__)
-
-@backend_blueprint.route('/test', methods=['GET'])
-def test():
-    bruh = "hi"
-    return bruh
-
